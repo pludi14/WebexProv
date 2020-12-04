11aa59e422eb9e93f970c8c8db45af4f39775320
Marcel <Marcel.pludra@tefsdfasf.de>
2020-12-04 16:56:04 +0100

    Update User fertig

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from webexapipkg.webexapi import Webexapi
+from webexapipkg.webexAPIException import WebexAPIException
 
-apikey="MTVhM2NlOGUtMTA1Mi00YWI0LTk2ZTMtNjE3YjM2NjVjY2VlYjRmNmIxNDItYjU1_PF84_7d2b833a-567c-442f-be92-af5fb4c537be"
+apikey="ZmVhN2NhODItMmRkYi00OWQ1LThlMjAtNmQxMWE5N2Y3ZDBjN2MwNDc1NGItMmVm_PF84_7d2b833a-567c-442f-be92-af5fb4c537be"
 api=Webexapi(apikey=apikey)
 
-api.listUser()
+#json = api.parseJSON("PUT_people.json")
+#print(json)
 
-
+try:
+    api.updateUser(personid="Y2lzY29zcGFyazovL3VzL1BFT1BMRS9iOGUwMWNjOS04OWM0LTQ1OTAtOWUxMS03NTU4NmVlNWQ5YmE", displayName="Bastian Schweinsteiger", licenses=["Y2lzY29zcGFyazovL3VzL0xJQ0VOU0UvN2QyYjgzM2EtNTY3Yy00NDJmLWJlOTItYWY1ZmI0YzUzN2JlOkVFXzQ5OGM3Zjg1LWZhMzUtNDM1ZC05OWVjLWE2ZWEyYTE5OGY3ZV9tYXJjZWxwbHVkcmEtZ2FzYW5kYm94LndlYmV4LmNvbQ"])
+except WebexAPIException as e:
+    print("Fehler: "+e.kwargs["text"])
 
--- webexapipkg/webexapi.py
+++ webexapipkg/webexapi.py
@@ -1,21 +1,52 @@
 import requests
 from webexapipkg.webexAPIException import WebexAPIException
 import json
+import os
+import pathlib
+import logging
+from http.client import HTTPConnection
+
 
 class Webexapi():
 
     def __init__(self, apikey=""):
         self.__apikey = apikey
+        self.__path = pathlib.Path(__file__).parent.absolute()
+        self.__workdir = pathlib.Path().absolute()
+        self.__referencePath = str(self.__workdir)+"/webexapipkg/reference/"
+
+    log = logging.getLogger('urllib3')
+    log.setLevel(logging.DEBUG)
+
+    # logging from urllib3 to console
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    log.addHandler(ch)
+
+    # print statements from `http.client.HTTPConnection` to console/stdout
+    HTTPConnection.debuglevel = 1
 
 
-    def __createRequest(self, urlZiel, methode, queryParameter):
+    def __createRequest(self, urlZiel, methode, queryParameter={}, querryData=""):
         apiUrl = "https://webexapis.com/v1/"+urlZiel
         httpHeaders = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.__apikey}
 
+        print(apiUrl)
+
         if methode=="GET":
             response = requests.get(url=apiUrl, headers=httpHeaders, params=queryParameter)
+            responsetext = json.loads(response.text)
+        if methode=="PUT":
+            response = requests.put(url=apiUrl, headers=httpHeaders, params=queryParameter, data=querryData)
+            responsetext = json.loads(response.text)
+        if methode=="POST":
+            response = requests.post(url=apiUrl, headers=httpHeaders, params=queryParameter)
+            responsetext = json.loads(response.text)
 
-        return response
+        if response.status_code == 200:
+            return responsetext
+        else:
+            raise WebexAPIException(statuscode=response.status_code, text=responsetext["message"]);
 
 
     def __setApikey(self, apikey=""):
@@ -25,10 +56,7 @@ class Webexapi():
         queryParams = {}
         response=self.__createRequest("people", "GET", queryParams)
         responsetext = json.loads(response.text)
-        if response.status_code==200:
-            return responsetext
-        else:
-            raise WebexAPIException(statuscode=response.status_code, text=responsetext["message"]);
+        return responsetext
 
 
     def insertUser(self):
@@ -41,14 +69,43 @@ class Webexapi():
         else:
             raise WebexAPIException(statuscode=response.status_code, text=responsetext["message"]);
 
+    def updateUser(self, **kwargs):
+
+        personID = kwargs.pop("personid")
+        data = {}
+
+        #print(kwargs)
+        for key in kwargs:
+            data[key]=kwargs[key]
+
+
+        #querrydata["emails"] = kwargs["emails"] if kwargs["emails"] else []
+        #querrydata["phoneNumbers"] = kwargs["phoneNumbers"] if kwargs["phoneNumbers"] else ""
+        #querrydata["extension"] = kwargs["extension"] if kwargs["extension"] else ""
+        #querrydata["locationId"] = kwargs["locationId"] if kwargs["locationId"] else ""
+        #querrydata["displayName"] = kwargs["displayName"] if kwargs["displayName"] else ""
+        #querrydata["firstName"] = kwargs["firstName"] if kwargs["firstName"] else ""
+        #querrydata["lastName"] = kwargs["lastName"] if kwargs["lastName"] else ""
+        #querrydata["avatar"] = kwargs["avatar"] if kwargs["avatar"] else ""
+        #querrydata["orgId"] = kwargs["orgId"] if kwargs["orgId"] else ""
+        #querrydata["roles"] = kwargs["roles"] if kwargs["roles"] else ""
+        #querrydata["licenses"] = kwargs["licenses"] if kwargs["licenses"] else ""
+
+        queryParams = {"callingData:" "true"}
+        jsondata=json.dumps(data)
+
+        response = self.__createRequest("people/"+personID, "PUT", querryData=jsondata)
+        print(response)
+
+    def parseJSON(self, datei):
+        with open(os.path.join(self.__referencePath,datei)) as file:
+            parsedJSON = json.load(file)
+            return parsedJSON
 
 
 if __name__=="__main__":
     api=Webexapi(apikey="ZmVhN2NhODItMmRkYi00OWQ1LThlMjAtNmQxMWE5N2Y3ZDBjN2MwNDc1NGItMmVm_PF84_7d2b833a-567c-442f-be92-af5fb4c537be")
-    try:
-        users=api.listUser()
-        print((users["items"]))
 
-    except WebexAPIException as e:
-        print("Fehler: "+e.kwargs["text"])
+
+
 
